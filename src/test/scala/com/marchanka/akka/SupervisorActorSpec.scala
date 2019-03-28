182c33338ea0a613d983ea417ca4384122d1dbcc
Vadzim_Marchanka <Vadzim_Marchanka@epam.com>
2019-03-28 09:44:27 +0300

    Added actor name for supervisor in test

--- src/test/scala/com/marchanka/akka/SupervisorActorSpec.scala
+++ src/test/scala/com/marchanka/akka/SupervisorActorSpec.scala
@@ -19,7 +19,7 @@ class SupervisorActorSpec(_system: ActorSystem) extends TestKit(_system)
 
   "A Supervisor Actor" should {
     "creates company actors that allows to create user actors" in {
-      val supervisor = system.actorOf(SupervisorActor.props)
+      val supervisor = system.actorOf(SupervisorActor.props, "supervisor")
 
       supervisor ! CreateCompany("Apple")
       supervisor ! CreateCompany("IBM")
@@ -27,20 +27,20 @@ class SupervisorActorSpec(_system: ActorSystem) extends TestKit(_system)
 
       Thread.sleep(1000)
 
-      system.actorSelection("/user/$a/Apple") ! CreateUser("user1")
-      system.actorSelection("/user/$a/Apple") ! CreateUser("user2")
-      system.actorSelection("/user/$a/Apple") ! CreateUser("user3")
-      system.actorSelection("/user/$a/Apple") ! CreateUser("user4")
+      system.actorSelection("/user/supervisor/Apple") ! CreateUser("user1")
+      system.actorSelection("/user/supervisor/Apple") ! CreateUser("user2")
+      system.actorSelection("/user/supervisor/Apple") ! CreateUser("user3")
+      system.actorSelection("/user/supervisor/Apple") ! CreateUser("user4")
 
-      system.actorSelection("/user/$a/IBM") ! CreateUser("user5")
-      system.actorSelection("/user/$a/IBM") ! CreateUser("user6")
-      system.actorSelection("/user/$a/IBM") ! CreateUser("user7")
-      system.actorSelection("/user/$a/IBM") ! CreateUser("user8")
+      system.actorSelection("/user/supervisor/IBM") ! CreateUser("user5")
+      system.actorSelection("/user/supervisor/IBM") ! CreateUser("user6")
+      system.actorSelection("/user/supervisor/IBM") ! CreateUser("user7")
+      system.actorSelection("/user/supervisor/IBM") ! CreateUser("user8")
 
-      system.actorSelection("/user/$a/Aeroflot") ! CreateUser("user9")
-      system.actorSelection("/user/$a/Aeroflot") ! CreateUser("user10")
-      system.actorSelection("/user/$a/Aeroflot") ! CreateUser("user11")
-      system.actorSelection("/user/$a/Aeroflot") ! CreateUser("user12")
+      system.actorSelection("/user/supervisor/Aeroflot") ! CreateUser("user9")
+      system.actorSelection("/user/supervisor/Aeroflot") ! CreateUser("user10")
+      system.actorSelection("/user/supervisor/Aeroflot") ! CreateUser("user11")
+      system.actorSelection("/user/supervisor/Aeroflot") ! CreateUser("user12")
 
 
     }
